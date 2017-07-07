12b6cb0f518144a72ee6245e0d3efb0ffc9e363c
jozhang <jozhang@eslite.com>
2017-07-07 14:45:06 +0800

    調整客戶檢視包裝內容的view

---
+++ EG_MagicCube/Models/ViewModel/WorksCreateViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EG_MagicCube.Models.ViewModel
+{
+    public class WorksCreateViewModel
+    {
+        [Display(Name = "作品序號")]
+        public string WorksNo { get; set; }
+        [Display(Name = "藝術家")]
+        public int AuthorsNo { get; set; }
+        [Display(Name = "作品名稱")]
+        public string WorksName { get; set; }
+        [Display(Name = "年代起")]
+        public string YearStart { get; set; }
+        [Display(Name = "年代迄")]
+        public string YearEnd { get; set; }
+        [Display(Name = "材質清單")]
+        public string MaterialsList { get; set; }
+
+
+        [Display(Name = "圖片")]
+        public string Img { get; set; }
+        public string Remarks { get; set; }
+        [Display(Name = "成本(萬)")]
+        public int Cost { get; set; }
+        [Display(Name = "定價(萬)")]
+        public int Price { get; set; }
+        [Display(Name = "定價日期")]
+        public System.DateTime PricingDate { get; set; }
+        [Display(Name = "毛利率")]
+        public double GrossMargin { get; set; }
+
+        [Display(Name = "作品類型")]
+        public List<SelectListItem> WorksStyle { get; set; }
+
+        [Display(Name = "作品風格")]
+        public List<SelectListItem> WorksGenre { get; set; }
+
+        public double Marketability { get; set; }
+        public double Packageability { get; set; }
+        public double Valuability { get; set; }
+        public double Artisticability { get; set; }
+
+        public string CreateUser { get; set; }
+        public System.DateTime CreateDate { get; set; }
+        public string ModifyUser { get; set; }
+        public Nullable<System.DateTime> ModifyDate { get; set; }
+
+        //public virtual ICollection<PackageItems> PackageItems { get; set; }
+        //public virtual ICollection<WorksAuthors> WorksAuthors { get; set; }
+        //public virtual ICollection<WorksFiles> WorksFiles { get; set; }
+        //public virtual ICollection<WorksModules> WorksModules { get; set; }
+        //public virtual ICollection<WorksPropGenre> WorksPropGenre { get; set; }
+        //public virtual ICollection<WorksPropOwner> WorksPropOwner { get; set; }
+        //public virtual ICollection<WorksPropStyle> WorksPropStyle { get; set; }
+        //public virtual ICollection<WorksPropWareType> WorksPropWareType { get; set; }
+    }
+}
