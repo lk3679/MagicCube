30ed7df67a1089c14375b8ccb24b7b638f2ccf25
yungchingliu <yungchingliu@eslite.com>
2017-07-10 09:30:21 +0800

    調整PackagesModel、WorksModel 加入搜尋用的WorksSearchModel、選單MenuModel

---
+++ EG_MagicCube/Models/MenuModel.cs
@@ -0,0 +1,304 @@
+﻿using EG_MagicCubeEntity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+using System.IO;
+using ImageMagick;
+namespace EG_MagicCube.Models
+{
+    public class MenuModel
+    {
+        /// <summary>
+        /// Menu類型
+        /// </summary>
+        public enum MenuClass {
+            /// <summary>
+            /// 全部
+            /// </summary>
+            ALL,
+            /// <summary>
+            /// 藝術家區域
+            /// </summary>
+            AuthorArea,
+            /// <summary>
+            /// 藝術家標籤
+            /// </summary>
+            AuthorTag,
+            /// <summary>
+            /// 量詞
+            /// </summary>
+            CountNoun,
+            /// <summary>
+            /// 作品類型
+            /// </summary>
+            Genre,
+            /// <summary>
+            /// 作品組件媒材
+            /// </summary>
+            Material,
+            /// <summary>
+            /// 作品所有人
+            /// </summary>
+            Owner,
+            /// <summary>
+            /// 作品風格
+            /// </summary>
+            Style,
+            /// <summary>
+            /// 作品庫別
+            /// </summary>
+            WareType };
+
+        /// <summary>
+        /// 取得Menu
+        /// </summary>
+        /// <param name="_MenuClass"></param>
+        /// <returns></returns>
+        public List<MenuViewModel> GetMenu(MenuClass _MenuClass)
+        {
+            List<MenuViewModel> _MenuViewModel = new List<MenuViewModel>();
+            string MenuClassName = Enum.GetName(typeof(MenuClass), _MenuClass);
+            using (var context = new EG_MagicCubeEntities())
+            {
+                switch (_MenuClass)
+                {
+                    case MenuClass.AuthorArea:
+                        _MenuViewModel.AddRange((from f in context.Menu_AuthorsArea
+                                                 select new MenuViewModel()
+                                                 { MenuClass = MenuClassName, MenuID = f.AuthorsAreaNo, MenuName = f.AuthorsAreaName }).ToList());
+                        break;
+                    case MenuClass.AuthorTag:
+                        _MenuViewModel.AddRange((from f in context.Menu_AuthorsTag
+                                                 select new MenuViewModel()
+                                                 { MenuClass = MenuClassName, MenuID = f.AuthorsTagNo, MenuName = f.AuthorsTagName }).ToList());
+                        break;
+                    case MenuClass.CountNoun:
+                        _MenuViewModel.AddRange((from f in context.Menu_CountNoun
+                                                 select new MenuViewModel()
+                                                 { MenuClass = MenuClassName, MenuID = f.CountNounNo, MenuName = f.CountNounName }).ToList());
+                        break;
+                    case MenuClass.Genre:
+                        _MenuViewModel.AddRange((from f in context.Menu_Genre
+                                                 select new MenuViewModel()
+                                                 { MenuClass = MenuClassName, MenuID = f.GenreNo, MenuName = f.GenreName }).ToList());
+                        break;
+                    case MenuClass.Material:
+                        _MenuViewModel.AddRange((from f in context.Menu_Material
+                                                 select new MenuViewModel()
+                                                 { MenuClass = MenuClassName, MenuID = f.MaterialNo, MenuName = f.MaterialName }).ToList());
+                        break;
+                    case MenuClass.Style:
+                        _MenuViewModel.AddRange((from f in context.Menu_Style
+                                                 select new MenuViewModel()
+                                                 { MenuClass = MenuClassName, MenuID = f.StyleNo, MenuName = f.StyleName }).ToList());
+                        break;
+                    case MenuClass.WareType:
+                        _MenuViewModel.AddRange((from f in context.Menu_WareType
+                                                 select new MenuViewModel()
+                                                 { MenuClass = MenuClassName, MenuID = f.WareTypeNo, MenuName = f.WareTypeName }).ToList());
+                        break;
+
+                    case MenuClass.Owner:
+                        _MenuViewModel.AddRange((from f in context.Menu_Owner
+                                                 select new MenuViewModel()
+                                                 { MenuClass = MenuClassName, MenuID = f.OwnerNo, MenuName = f.OwnerName }).ToList());
+                        break;
+                }
+                
+            }
+            return _MenuViewModel;
+        }
+
+        /// <summary>
+        /// 新增Menu
+        /// </summary>
+        /// <param name="_MenuClass">Meun類型</param>
+        /// <param name="_MenuViewModelList"></param>
+        /// <returns></returns>
+        public bool InsertMenu(MenuClass _MenuClass, List<MenuViewModel> _MenuViewModelList)
+        {
+            string MenuClassName = Enum.GetName(typeof(MenuClass), _MenuClass);
+            using (var context = new EG_MagicCubeEntities())
+            {
+                switch (_MenuClass)
+                {
+                    case MenuClass.AuthorArea:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            context.Menu_AuthorsArea.Add(new Menu_AuthorsArea() { AuthorsAreaNo = _MenuViewModel.MenuID, AuthorsAreaName = _MenuViewModel.MenuName });
+                        }
+                        break;
+                    case MenuClass.AuthorTag:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            context.Menu_AuthorsTag.Add(new Menu_AuthorsTag() { AuthorsTagNo = _MenuViewModel.MenuID, AuthorsTagName = _MenuViewModel.MenuName });
+                        }
+                        break;
+                    case MenuClass.CountNoun:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            context.Menu_CountNoun.Add(new Menu_CountNoun() { CountNounNo = _MenuViewModel.MenuID, CountNounName = _MenuViewModel.MenuName });
+                        }
+                        break;
+                    case MenuClass.Genre:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            context.Menu_Genre.Add(new Menu_Genre() { GenreNo = _MenuViewModel.MenuID, GenreName = _MenuViewModel.MenuName });
+                        }
+                        break;
+                    case MenuClass.Material:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            context.Menu_Material.Add(new Menu_Material() { MaterialNo = _MenuViewModel.MenuID, MaterialName = _MenuViewModel.MenuName });
+                        }
+                        break;
+                    case MenuClass.Style:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            context.Menu_Style.Add(new Menu_Style() { StyleNo = _MenuViewModel.MenuID, StyleName = _MenuViewModel.MenuName });
+                        }
+                        break;
+                    case MenuClass.WareType:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            context.Menu_WareType.Add(new Menu_WareType() { WareTypeNo = _MenuViewModel.MenuID, WareTypeName = _MenuViewModel.MenuName });
+                        }
+                        break;
+
+                    case MenuClass.Owner:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            context.Menu_Owner.Add(new Menu_Owner() { OwnerNo = _MenuViewModel.MenuID, OwnerName = _MenuViewModel.MenuName });
+                        }
+                        break;
+                }
+                if (context.SaveChanges() == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool DeleteMenu(MenuClass _MenuClass, List<MenuViewModel> _MenuViewModelList)
+        {
+
+            string MenuClassName = Enum.GetName(typeof(MenuClass), _MenuClass);
+            using (var context = new EG_MagicCubeEntities())
+            {
+                switch (_MenuClass)
+                {
+                    case MenuClass.AuthorArea:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            var delobj = context.Menu_AuthorsArea.FirstOrDefault(x => x.AuthorsAreaNo == _MenuViewModel.MenuID);
+                            if (delobj != null)
+                            {
+                                context.Menu_AuthorsArea.Remove(delobj);
+                            }
+                        }
+                        break;
+                    case MenuClass.AuthorTag:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            var delobj = context.Menu_AuthorsTag.FirstOrDefault(x => x.AuthorsTagNo == _MenuViewModel.MenuID);
+                            if (delobj != null)
+                            {
+                                context.Menu_AuthorsTag.Remove(delobj);
+                            }
+                        }
+                        break;
+                    case MenuClass.CountNoun:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            var delobj = context.Menu_CountNoun.FirstOrDefault(x => x.CountNounNo == _MenuViewModel.MenuID);
+                            if (delobj != null)
+                            {
+                                context.Menu_CountNoun.Remove(delobj);
+                            }
+                        }
+                        break;
+                    case MenuClass.Genre:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            var delobj = context.Menu_Genre.FirstOrDefault(x => x.GenreNo == _MenuViewModel.MenuID);
+                            if (delobj != null)
+                            {
+                                context.Menu_Genre.Remove(delobj);
+                            }
+                        }
+                        break;
+                    case MenuClass.Material:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            var delobj = context.Menu_Material.FirstOrDefault(x => x.MaterialNo == _MenuViewModel.MenuID);
+                            if (delobj != null)
+                            {
+                                context.Menu_Material.Remove(delobj);
+                            }
+                        }
+                        break;
+                    case MenuClass.Style:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            var delobj = context.Menu_Style.FirstOrDefault(x => x.StyleNo == _MenuViewModel.MenuID);
+                            if (delobj != null)
+                            {
+                                context.Menu_Style.Remove(delobj);
+                            }
+                        }
+                        break;
+                    case MenuClass.WareType:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            var delobj = context.Menu_WareType.FirstOrDefault(x => x.WareTypeNo == _MenuViewModel.MenuID);
+                            if (delobj != null)
+                            {
+                                context.Menu_WareType.Remove(delobj);
+                            }
+                        }
+                        break;
+                    case MenuClass.Owner:
+                        foreach (MenuViewModel _MenuViewModel in _MenuViewModelList)
+                        {
+                            var delobj = context.Menu_Owner.FirstOrDefault(x => x.OwnerNo == _MenuViewModel.MenuID);
+                            if (delobj != null)
+                            {
+                                context.Menu_Owner.Remove(delobj);
+                            }
+                        }
+                        break;
+                }
+                if (context.SaveChanges() == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// 選項
+    /// </summary>
+    public class MenuViewModel
+    {
+        /// <summary>
+        /// Menu類型
+        /// </summary>
+        public string MenuClass { get; set; }
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int MenuID { get; set; }
+        /// <summary>
+        /// 名稱
+        /// </summary>
+        public string MenuName { get; set; }
+    }
+}
--- EG_MagicCube/Models/PackagesModel.cs
+++ EG_MagicCube/Models/PackagesModel.cs
@@ -3,14 +3,72 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
-
+using System.Drawing;
+using System.IO;
+using ImageMagick;
+using ZXing;
 namespace EG_MagicCube.Models
 {
-    [MetadataType(typeof(PackagesModelMetaData))]
-    public partial class PackagesModel:Packages
+    //[MetadataType(typeof(PackagesModelMetaData))]
+    public partial class PackagesModel//:Packages
     {
+        /// <summary>
+        /// 包裝序號
+        /// </summary>
+        [DisplayName("包裝序號")]
+        public Guid PackagesNo { get; set; }
+        /// <summary>
+        /// 包裝名稱
+        /// </summary>
+        [DisplayName("包裝名稱")]
+        public string PackagesName { get; set; }
+        /// <summary>
+        /// 截止日期
+        /// </summary>
+        [DisplayName("截止日期")]
+        public Nullable<DateTime> EndDate { get; set; }
+        /// <summary>
+        /// 打包日期
+        /// </summary>
+        [DisplayName("打包日期")]
+        public Nullable<DateTime> PackingDate { get; set; }
+        /// <summary>
+        /// 建立日期
+        /// </summary>
+        [DisplayName("建立日期")]
+        public DateTime CreateDate { get; set; }
+        /// <summary>
+        /// 建立者
+        /// </summary>
+        [DisplayName("建立者")]
+        public string CreateUser { get; set; }
+        /// <summary>
+        /// 修改者
+        /// </summary>
+        [DisplayName("修改者")]
+        public string ModifyUser { get; set; }
+        /// <summary>
+        /// 修改日期
+        /// </summary>
+        [DisplayName("修改日期")]
+        public Nullable<DateTime> ModifyDate { get; set; }
+        /// <summary>
+        /// 查詢條件
+        /// </summary>
+        [DisplayName("查詢條件")]
+        public string SearchJson { get; set; }
+        /// <summary>
+        /// 備註
+        /// </summary>
+        public string PackagesMemo { get; set; }
+        /// <summary>
+        /// 作品編號清單
+        /// </summary>
+        [DisplayName("作品編號清單")]
+        public List<string> WorksNos { get; set; }
         #region Methods
         #region Create
         /// <summary>
@@ -21,7 +79,28 @@ namespace EG_MagicCube.Models
         {
             using (var context = new EG_MagicCubeEntities())
             {
-                context.Packages.Add(this);
+                Packages _Package = new Packages();
+                _Package.PackagesNo = Guid.NewGuid();
+                _Package.PackagesName = this.PackagesName;
+                _Package.EndDate = this.EndDate;
+                _Package.PackingDate = this.PackingDate;
+                _Package.PackagesMemo = this.PackagesMemo;
+                _Package.SearchJson = this.SearchJson;
+                _Package.CreateDate = DateTime.Now;
+                _Package.CreateUser = "";
+                _Package.ModifyDate = DateTime.Now;
+                _Package.ModifyUser = "";
+                foreach (string WorksNo in WorksNos)
+                {
+                    _Package.PackageItems.Add(new EG_MagicCubeEntity.PackageItems()
+                    {
+                        PackagesNo = _Package.PackagesNo,
+                        WorksNo = Guid.Parse(WorksNo),
+                        JoinDate = DateTime.Now,
+                        IsJoin = "Y"
+                    });
+                }
+                context.Packages.Add(_Package);
                 if (context.SaveChanges() == 0)
                 {
                     return false;
@@ -35,24 +114,11 @@ namespace EG_MagicCube.Models
         /// <summary>
         /// 取得包裝
         /// </summary>
-        public IQueryable<Packages> All()
+        public List<Packages> All(int PageIndex = 1, int PageSize = 10)
         {
             using (var context = new EG_MagicCubeEntities())
             {
-                return context.Packages;
-            }
-        }
-
-        /// <summary>
-        /// 以包裝編號取得包裝品
-        /// </summary>
-        /// <param name="PackagesNo">包裝編號</param>
-        /// <returns></returns>
-        public Packages GetPackagesByPG_No(string PackagesNo)
-        {
-            using (var context = new EG_MagicCubeEntities())
-            {
-                return context.Packages.FirstOrDefault(x => x.PackagesNo == Guid.Parse(PackagesNo));
+                return context.Packages.OrderBy(c => c.PackagesNo).Skip((PageIndex * PageSize) - PageSize).Take(PageSize).ToList();
             }
         }
         #endregion
@@ -70,11 +136,13 @@ namespace EG_MagicCube.Models
                 var oldPackages = context.Packages.First(x => x.PackagesNo == newPackages.PackagesNo);
                 oldPackages.PackagesName = newPackages.PackagesName;
                 oldPackages.PackingDate = newPackages.PackingDate;
-                oldPackages.PackageItems = newPackages.PackageItems;
                 oldPackages.EndDate = newPackages.EndDate;
                 oldPackages.ModifyUser = newPackages.ModifyUser;
                 oldPackages.ModifyDate = newPackages.ModifyDate;
                 oldPackages.PackagesMemo = newPackages.PackagesMemo;
+                oldPackages.SearchJson = newPackages.SearchJson;
+                
+
                 if (context.SaveChanges() == 0)
                 {
                     return false;
@@ -109,54 +177,41 @@ namespace EG_MagicCube.Models
 
         #endregion
     }
-    public partial class PackagesModelMetaData
+    public partial class QRCodeWriter
     {
-        #region Properties
-        [Required]
-        [DisplayName("作品序號")]
-        public string WorksNo { get; set; }
-        [Required]
-        [DisplayName("物料代碼")]
-        public string MaterialsID { get; set; }
-        [Required]
-        [DisplayName("藝術家編號")]
-        public string AuthorsNo { get; set; }
-        [Required]
-        [DisplayName("作品名稱")]
-        public string WorksName { get; set; }
-        [Required]
-        [DisplayName("作品起始年分")]
-        public string YearStart { get; set; }
-        [Required]
-        [DisplayName("作品結束年分")]
-        public string YearEnd { get; set; }
-        [Required]
-        [DisplayName("備註")]
-        public string Remarks { get; set; }
-        [Required]
-        [DisplayName("成本價")]
-        public string Cost { get; set; }
-        [Required]
-        [DisplayName("定價")]
-        public string Price { get; set; }
-        [Required]
-        [DisplayName("定價時間")]
-        public string PricingDate { get; set; }
-        [Required]
-        [DisplayName("毛利率")]
-        public string GrossMargin { get; set; }
-        [Required]
-        [DisplayName("市場性")]
-        public string Marketability { get; set; }
-        [Required]
-        [DisplayName("包裹性")]
-        public string Packageability { get; set; }
-        [Required]
-        [DisplayName("增值性")]
-        public string Valuability { get; set; }
-        [Required]
-        [DisplayName("藝術性")]
-        public string Artisticability { get; set; }
-        #endregion
+        private string _Base64String = "";
+        public string Base64String { get { return _Base64String; } }
+        public QRCodeWriter(string _Contens)
+        {
+            DrawQRcodeToImgBase64sting(_Contens);
+        }
+        public void DrawQRcodeToImgBase64sting(string _Contens)
+        {
+            MemoryStream ms_qr = new MemoryStream();
+            //產生256*2562的QRCode PNG圖片物件
+            ((System.Drawing.Image)(new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Renderer = new ZXing.Rendering.BitmapRenderer(),
+                Options = new ZXing.QrCode.QrCodeEncodingOptions
+                {
+                    Height = 256,
+                    Width = 256,
+                    DisableECI = true,
+                    CharacterSet = "UTF-8",
+                    ErrorCorrection = ZXing.QrCode.Internal.ErrorCorrectionLevel.M,
+                    Margin = 1,
+                }
+            }).Write(_Contens)).Save(ms_qr, System.Drawing.Imaging.ImageFormat.Png);
+
+            MemoryStream ms_mini = new MemoryStream();
+            //將圖片轉成png8,壓縮率70，
+            (new MagickImage(ms_qr.ToArray()) { Format = MagickFormat.Png8, Quality = 70 }).Write(ms_mini);
+            //轉成Base64
+            _Base64String = Convert.ToBase64String(ms_mini.ToArray());
+
+            ms_qr.Dispose();
+            ms_mini.Dispose();
+        }
     }
 }
