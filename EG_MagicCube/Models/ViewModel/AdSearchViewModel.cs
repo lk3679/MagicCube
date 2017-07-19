1d34f5b262d9330758ac5064e614525949145bc3
yungchingliu <yungchingliu@eslite.com>
2017-07-19 18:01:25 +0800

    調整文字說明

--- EG_MagicCube/Models/ViewModel/AdSearchViewModel.cs
+++ EG_MagicCube/Models/ViewModel/AdSearchViewModel.cs
@@ -18,7 +18,7 @@ namespace EG_MagicCube.Models.ViewModel
         [Display(Name = "預算")]
         public int Budget { get; set; } = 0;
 
-        [Display(Name = "藝術家編號清單")]
+        [Display(Name = "藝術家")]
         public List<string> AuthorNoList { get; set; } = new List<string>();
 
         [Display(Name = "定價上限")]
