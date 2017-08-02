9b65a72faa43d1936c737c62a1cb150fe5bd5917
yungchingliu <yungchingliu@eslite.com>
2017-08-02 16:36:11 +0800

    調整ckeditor 工具列

--- EG_MagicCube/Scripts/ckeditor/config.js
+++ EG_MagicCube/Scripts/ckeditor/config.js
@@ -3,8 +3,15 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+    config.height  = 120; 
+    config.toolbar = [
+        { name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
+        { name: 'editing', items: ['Scayt'] },
+        { name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+        { name: 'insert', items: ['Image', 'Table', 'HorizontalRule', 'SpecialChar'] },
+         { name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat'] }, { name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote'] }, { name: 'styles', items: ['Styles', 'Format'] },
+         { name: 'tools', items: ['Maximize'] },{ name: 'document', items: ['Source'] },
+        '/',
+    ];
 };
