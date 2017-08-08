c02c4a4c82094f121b698b8427b13a4f0295e0e8
yungchingliu <yungchingliu@eslite.com>
2017-08-08 11:02:41 +0800

    新增公式提示及計算、數量及等級改為不輸入

---
+++ EG_MagicCube/Scripts/eslitemg.js
@@ -0,0 +1,11 @@
+﻿
+//---------------------數字、日期類型的input禁用滾輪------------------------
+$('form').on('focus', 'input[type=number],input[type=date]', function (e) {
+    $(this).on('mousewheel.disableScroll', function (e) {
+        e.preventDefault()
+    })
+})
+$('form').on('blur', 'input[type=number],input[type=date]', function (e) {
+    $(this).off('mousewheel.disableScroll')
+})
+//-------------------------------------------------------------------------
